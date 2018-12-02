db64d3cf0a820fff89492ef85d283a0325b5ca56
moshe-olshansky <40348083+moshe-olshansky@users.noreply.github.com>
2018-12-02 19:50:54 +1100

    Add files via upload

--- bestEigRscript_h5_1.R
+++ bestEigRscript_h5_1.R
@@ -1,5 +1,5 @@
 require(Matrix)
-source("bestEigen2.R")
+source("bestEigen1.R")
 args <- commandArgs(trailingOnly = TRUE)
 
 fin <- args[1]
@@ -8,16 +8,22 @@ binsize <- as.integer(args[3])
 if (length(args) >= 4) tol  <- as.numeric(args[4])
 if (length(args) >= 5) maxiter <- as.integer(args[5])
 
+t0 <- Sys.time()
 y <- scan(fin,what=list(s=integer(),e=integer(),v=double()))
+#y <- scan(fin,what=list(s=double(),e=double(),v=double()))
 y$v[is.na(y$v)] <- 0
 i <- y$s/binsize+1
 j <- y$e/binsize+1
 x <- sparseMatrix(i=i,j=j,x=y$v,symmetric=TRUE)
-if (length(args) == 3) res <- bestEigen1(x)
-if (length(args) == 4) res <- bestEigen1(x,tol=tol)
-if (length(args) == 5) res <- bestEigen1(x,maxiter=maxiter,tol=tol)
+rm(y)
+for (i in 1:5) gc()
+t1 <- Sys.time()
+if (length(args) == 3) res <- bestEigen(x)
+if (length(args) == 4) res <- bestEigen(x,tol=tol)
+if (length(args) == 5) res <- bestEigen(x,maxiter=maxiter,tol=tol)
 n <- length(res$v)
 z <- cbind(as.integer(((1:n)-0.5)*binsize),res$v)
 write.table(z,fout,quote=FALSE,row.names=FALSE,col.names=FALSE)
-writeLines(paste("lam1 =",res$lam1,",er =",res$er,",lam2 =",res$lam2," niter=",res$niter),paste0(fout,".out"))
+t2 <- Sys.time()
+writeLines(paste("lam1 =",res$lam1,"\ner =",res$er,"\nlam2 =",res$lam2,"\nniter=",res$niter,"\ntime to read and create matrix =",format(t1-t0,format="%H:%M:%S"), "\ntime to compute eigenvector = ",format(t2-t1,format="%H:%M:%S")),paste0(fout,".out"))
 
